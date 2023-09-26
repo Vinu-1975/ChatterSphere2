88fcc886eea237ee120ee1f005461067b85fc5bf
vinayakan <vinayakanvs2003@gmail.com>
2023-09-26 20:48:34 +0530

    fixed create group chat bug

--- server/controller/chatController.js
+++ server/controller/chatController.js
@@ -75,34 +75,6 @@ module.exports.fetchChats = async (req,res) => {
 }
 
 module.exports.createGroupChat = async (req,res) => {
-    
-    // if(!req.body.users || !req.body.name){
-    //     return res.status(400).send({message:"Please Fill all the fields"})
-    // }
-    // var users = JSON.parse(req.body.users);
-    // if(users.length < 2){
-    //     return res.status(400).send("More than 2 users are required to form a group chat")
-    // }
-    // users.push(req.user)
-    // try{
-    //     const groupChat = await Chat.create({
-    //         chatName: req.body.name,
-    //         users:users,
-    //         isGroupChat:true,
-    //         groupAdmin:req.user
-    //     })
-
-    //     const fullGroupChat = await Chat.findOne({_id:groupChat._id})
-    //     .populate("users","-password")
-    //     .populate("groupAdmin","-password")
-
-    //     res.status(200).json(fullGroupChat)
-
-    // }catch(error){
-    //     res.status(400)
-    //     throw new Error(error.message)
-    // }
-
 
 
     try {
@@ -118,6 +90,12 @@ module.exports.createGroupChat = async (req,res) => {
             const mime = req.file.mimetype;
             const base64 = req.file.buffer.toString('base64');
             const base64URL = `data:${mime};base64,${base64}`;
+            cloudinary.config({
+                cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+                api_key: process.env.CLOUDINARY_API_KEY,
+                api_secret: process.env.CLOUDINARY_API_SECRET,
+                secure: true,
+            });
             const result = await cloudinary.uploader.upload(base64URL, {
                 public_id: `group-${Date.now()}`,  // Unique ID based on timestamp
                 // upload_preset: 'chat-app'  // Uncomment if you have an upload preset
