9a4e1eafc21e84a26d54316be333f184ad94980b
vinayakan <vinayakanvs2003@gmail.com>
2023-09-27 21:29:16 +0530

    ready for deployment

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000"
+export const host = "https://chattersphere2.onrender.com"
 export const registerRoute = `${host}/api/auth/register`
 export const loginRoute = `${host}/api/auth/login`
 export const allUsers = `${host}/api/auth`
